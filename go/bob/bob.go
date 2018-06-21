c7bb638187f588f325aa5547ff9bc13c9d531239
Chris Steinmeyer <csteinmeyer@gmail.com>
2018-06-20 18:55:10 -0600

    a clean Bob

--- go/bob/bob.go
+++ go/bob/bob.go
@@ -1,102 +1,67 @@
-// This is a "stub" file.  It's a little start on your solution.
-// It's not a complete solution though; you have to write some code.
-
-// Package bob should have a package comment that summarizes what it's about.
-// https://golang.org/doc/effective_go.html#commentary
 package bob
 
-/*
-Bob answers 'Sure.' if you ask him a question.
-
-He answers 'Whoa, chill out!' if you yell at him.
-
-He answers 'Calm down, I know what I'm doing!' if you yell a question at him.
-
-He says 'Fine. Be that way!' if you address him without actually saying
-anything.
-
-He answers 'Whatever.' to anything else.
+/* Checks that Bob replies correctly when asked a variety of questions.
+If 'remark' is yelling, is a question and is sayingWords then it replies with:
+	"Calm down, I know what I'm doing!"
+If 'remark' is a question then it replies with:
+	"Sure."
+If 'remark' is a silence (no characters) then it replies with:
+	"Fine. Be that way!"
+If 'remark' is a yelling actual words then it replies with:
+	"Whoa, chill out!"
+Everything else replies with:
+	"Whatever."
 */
 
 import (
-	"fmt"
 	"regexp"
 	"strings"
 )
 
-//} else if strings.HasSuffix(remark, "!") || AllCaps(remark) {
-
-// Hey should have a comment documenting it.
+// Hey - executes the Bob-rules for replies as defined above.
 func Hey(remark string) string {
-	fmt.Println(remark)
 	remark = strings.TrimSpace(remark)
-	fmt.Println(remark)
 	var response string
-
-	var sayingSomething, sayingWords, yelling, question = IsSayingSomething(remark), IsSayingWords(remark), IsYelling(remark), IsQuestion(remark)
-	fmt.Println(sayingSomething, yelling, question)
+	var silence, sayingWords, yelling, question = isEmpty(remark), isSayingWords(remark), isYelling(remark), isQuestion(remark)
 
 	if yelling && question && sayingWords {
 		response = "Calm down, I know what I'm doing!"
-		fmt.Println(response)
 	} else if question {
 		response = "Sure."
-		fmt.Println(response)
-	} else if !sayingSomething {
+	} else if silence {
 		response = "Fine. Be that way!"
-		fmt.Println(response)
 	} else if yelling && sayingWords {
 		response = "Whoa, chill out!"
-		fmt.Println(response)
 	} else {
 		response = "Whatever."
-		fmt.Println(response)
 	}
 
 	return response
 }
 
-// IsSayingSomething - checks if 'remark' is in all caps
-func IsSayingSomething(remark string) bool {
-	match, _ := regexp.MatchString("\\w", remark)
-	return match
+// isEmpty - checks if 'remark' contains any characters
+func isEmpty(remark string) bool {
+	return remark == ""
 }
 
-// IsSayingWords - checks if 'remark' is in all caps
-func IsSayingWords(remark string) bool {
+// isSayingWords - checks if 'remark' are word characters
+func isSayingWords(remark string) bool {
 	match, _ := regexp.MatchString("[a-zA-Z]", remark)
 	return match
 }
 
-// IsYelling - checks if 'remark' is in all caps
-func IsYelling(remark string) bool {
+// isYelling - checks if 'remark' is in all caps
+func isYelling(remark string) bool {
 	return strings.ToUpper(remark) == remark
-	// //match, _ := regexp.MatchString("^[A-Z0-9\\s,]+[!\\.\\?]?$", remark)
-	// allCaps := strings.ToUpper(remark) == remark
-	// endsWithExcl := strings.HasSuffix(remark, "!")
-	// isWords := isWords(remark)
-	// //areWords := strings.HasSuffix(remark, "!")
-	// fmt.Print("capsMatch:")
-	// fmt.Println(allCaps)
-	// fmt.Print("endsWith !")
-	// fmt.Println(endsWithExcl)
-	// fmt.Print("iswords:")
-	// fmt.Println(isWords)
-
-	// //match, _ := regexp.MatchString("^([A-Z0-9\\s,]+[!\\.\\?]?$)", remark)
-	// fmt.Println("======================")
-	// fmt.Println(allCaps && endsWithExcl, isWords)
-	// //return match || match && strings.HasSuffix(remark, "!")
-	// return (allCaps && endsWithExcl && isWords)
 }
 
-// IsWords - is it a question
+// isWords - contains word charaters
 func isWords(remark string) bool {
 	match, _ := regexp.MatchString("^([\\w]+$)", remark)
 	return match
 }
 
-// IsQuestion - is it a question
-func IsQuestion(remark string) bool {
+// isQuestion - Checks if remark ends with a '?'
+func isQuestion(remark string) bool {
 	return strings.HasSuffix(remark, "?")
 }
