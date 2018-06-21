5bf98dc3970c8815fa4bc74a8c1f205313bc5965
Chris Steinmeyer <csteinmeyer@gmail.com>
2018-06-20 23:04:32 -0600

    now with recursion

--- go/acronym/acronym.go
+++ go/acronym/acronym.go
@@ -7,11 +7,18 @@ import (
 
 // Abbreviate - Returns the acronyms of 's'
 func Abbreviate(s string) string {
-	var acro string
 	wordsPattern := regexp.MustCompile("\\w+")
 	words := wordsPattern.FindAllString(s, -1)
-	for _, word := range words {
-		acro += strings.ToUpper(string(word[0]))
+	return BuildAcro(words, "")
+}
+
+// BuildAcro - recursively builds an acronym from the the words in 'words'
+func BuildAcro(words []string, acro string) string {
+	if len(words) == 0 {
+		return acro
 	}
-	return acro
+	first, rest := words[0], words[1:]
+	acro += strings.ToUpper(string(first[0]))
+
+	return BuildAcro(rest, acro)
 }
