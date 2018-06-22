978415c439f8f4b64d5a1d31c1d53aca38efb4dd
Chris Steinmeyer <csteinmeyer@gmail.com>
2018-06-21 21:33:01 -0700

    a little refactor for raindrops

--- go/raindrops/raindrops.go
+++ go/raindrops/raindrops.go
@@ -8,20 +8,26 @@ import (
 //Convert - converts numbers to raindrops :mindblown:
 func Convert(num int) string {
 	rain := ""
-	if math.Remainder(float64(num), 3) == 0 {
-		rain += "Pling"
-	}
-
-	if math.Remainder(float64(num), 5) == 0 {
-		rain += "Plang"
-	}
-
-	if math.Remainder(float64(num), 7) == 0 {
-		rain += "Plong"
-	}
+	rain += AddStringIfFactor(num, 3, "Pling")
+	rain += AddStringIfFactor(num, 5, "Plang")
+	rain += AddStringIfFactor(num, 7, "Plong")
 
 	if rain == "" {
 		rain = strconv.Itoa(num)
 	}
 	return rain
+}
+
+//AddStringIfFactor - returns 'raindrop' if 'num' is evenly divisible by 'divisor'
+func AddStringIfFactor(num int, divisor int, raindrop string) string {
+	rain := ""
+	if Remainder(num, divisor) == 0 {
+		rain += raindrop
+	}
+	return rain
+}
+
+//Remainder - returns the remainder of 'num' divided by 'divisor'
+func Remainder(num int, divisor int) float64 {
+	return math.Remainder(float64(num), float64(divisor))
 }
