b112590321d71c49dc5e1f3df84554cf744c19fb
Chris Steinmeyer <csteinmeyer@gmail.com>
2018-06-27 10:48:41 -0700

    go isogram with runes

--- go/isogram/isogram.go
+++ go/isogram/isogram.go
@@ -1,22 +1,22 @@
 package isogram
 
 import (
-	"strings"
+	"unicode"
 )
 
 //IsIsogram - determines whether or not 'word' is an isogram
 func IsIsogram(word string) bool {
 	isogram := true
-	var values = map[string]string{}
+	var values = map[rune]rune{}
 
-	for i := 0; i < len(word); i++ {
-		letter := strings.ToLower(string(word[i]))
-		if letter == " " || letter == "-" {
-			continue //skip them as they don't matter in an isogram
-		} else if _, ok := values[letter]; ok {
+	for _, runeValue := range word {
+		runeValue = unicode.ToUpper(runeValue)
+		if runeValue == 45 || runeValue == 32 {
+			continue //skip " " (32) and "-" (45) as they don't matter in an isogram
+		} else if _, ok := values[runeValue]; ok {
 			isogram = false
 		} else {
-			values[letter] = letter
+			values[runeValue] = runeValue
 		}
 	}
 	return isogram
