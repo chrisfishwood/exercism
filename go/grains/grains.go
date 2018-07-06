87f2bfac45f5fba060221a515de9f41ea35f483f
Chris Steinmeyer <csteinmeyer@gmail.com>
2018-07-06 17:07:15 -0500

    Square works

--- go/grains/grains.go
+++ go/grains/grains.go
@@ -8,27 +8,26 @@ import (
 // Square -
 func Square(val int) (uint64, error) {
 	fmt.Printf("Square called with %d\n", val)
-	var total uint64 = 1
 	var previous uint64 = 1
-	if val == 0 {
-		return 0, errors.New("0 is bad, mkay")
+	var grains uint64
+	if val <= 0 || val > 64 {
+		return 0, errors.New("number out of range (1-64)")
 	} else if val == 1 {
-		return total, nil
+		return 1, nil
 	} else {
 		for i := 2; i <= val; i++ {
-			v := 2 * previous
-			previous = v
-			total = total + v
-			fmt.Printf("Previous: %d, Total: %d\n", previous, total)
+			grains = 2 * previous
+			previous = grains
 		}
 	}
-	return uint64(total), errors.New("Some error")
+	return uint64(grains), nil
 }
 
 // Total -
 func Total() uint64 {
 	//var squerror error
 	var total uint64 = 1
+	fmt.Println("calling total")
 	total, _ = Square(64)
 	return total
 }
