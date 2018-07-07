263270b6946a4d8db23b7126eb6d2597b6db869a
Chris Steinmeyer <csteinmeyer@gmail.com>
2018-07-07 12:37:51 -0500

    exercism go grains - fast

--- go/grains/grains.go
+++ go/grains/grains.go
@@ -4,28 +4,48 @@ import (
 	"errors"
 )
 
-// Square -
+// Square - Returns the number of grains on a given square on the chessboard
 func Square(val int) (uint64, error) {
-	var previous uint64 = 1
+
 	var grains uint64
-	if val <= 0 || val > 64 {
+
+	if val < 1 || val > 64 {
 		return 0, errors.New("number out of range (1-64)")
-	} else if val == 1 {
-		return 1, nil
-	} else {
-		for i := 2; i <= val; i++ {
-			grains = 2 * previous
-			previous = grains
-		}
 	}
-	return uint64(grains), nil
+
+	grains = 1 << uint64(val-1)
+	return grains, nil
 }
 
-// Total -
+// Total - returns the total number of grains on the chessboard
 func Total() uint64 {
-	var val uint64 = 1
+	/*
+		I chose the readable solution over the fast solution.
+
+		`total = 1<<uint64(64) - 1` instead of the for loop had the following performance...
+
+		~/projects/chrisfishwood/exercism/go/grains (master) $ go test --bench .
+		goos: darwin
+		goarch: amd64
+		BenchmarkSquare-8       30000000                38.4 ns/op
+		BenchmarkTotal-8        2000000000               0.28 ns/op
+
+		...which was smoking fast, but I found it far less readable to someone coming into this code.
+		I also like that `Total` uses `Square` as part of it's solution so a test for `Total` will also test `Square`.
+
+		The for loop solution is definitely slower, but at ~80ns it's still pretty darn performant.
+
+		~/projects/chrisfishwood/exercism/go/grains (master) $ go test --bench .
+		goos: darwin
+		goarch: amd64
+		BenchmarkSquare-8       30000000                38.2 ns/op
+		BenchmarkTotal-8        20000000                79.6 ns/op
+	*/
+
+	var val uint64
 	var total uint64
-	for i := 0; i <= 64; i++ {
+
+	for i := 1; i <= 64; i++ {
 		val, _ = Square(i)
 		total = total + val
 	}
